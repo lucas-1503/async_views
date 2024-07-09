b931407e320d34ff2e90050fd91c561dcb6ef801
Lucas Valentin Montezuma <lucasvmontezuma@gmail.com>
2024-07-09 11:46:22 -0300

    async-views

---
+++ asyncviews/views.py
@@ -0,0 +1,41 @@
+import time
+from django.http import JsonResponse, HttpResponse
+import asyncio
+import httpx
+from time import sleep
+
+def api(request):
+    time.sleep(1)
+    payload = {"messege":"Hello World"}
+
+    if 'task_id' in request.GET:
+        payload['task_id'] = request.GET['task_id']
+
+    return JsonResponse(payload)
+
+async def http_call_async():
+    for num in range(1,6):
+        await asyncio.sleep(1)
+        print(num)
+    async with httpx.AsyncClient() as client:
+        r = await client.get("https://httpbin.org")
+        print(r)
+
+async def async_view(request):
+    loop = asyncio.get_event_loop()
+    loop.create_task(http_call_async())
+    return HttpResponse("Non blocking Http request")
+ 
+
+
+def call_sync():
+    for num in range(1,6):
+        sleep(1)
+        print(num)
+    r = httpx.get('https://httpbin.org')
+    print(r)
+
+
+def sync_view(request):
+    call_sync()
+    return HttpResponse("Blocking HTTP request")
